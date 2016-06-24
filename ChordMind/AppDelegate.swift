51b37e819de0829f966996be950595534661a458
David Brown <apple@davidb.org>
2016-06-24 16:15:33 -0600

    Initial Commit

---
+++ ChordMind/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ChordMind
+//
+//  Created by David Brown on 6/24/16.
+//  Copyright © 2016 David Brown. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
